c2e389c56c534874a2dd2f1e0b6a7badb9eff329
Dmitry Dorogoy <Dmitrii_Dorogoi@epam.com>
2024-06-05 08:54:21 +0200

    GetTrackCount function added

---
+++ DimonSmart.RegexUnitTester.TestAdapter/RegexInformation.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace DimonSmart.RegexUnitTester.TestAdapter;
+
+public static class RegexInformation
+{
+    public static int? GetTrackCount(string pattern)
+    {
+        var regex = new Regex(pattern);
+        var regexType = typeof(Regex);
+        var regexCodeField = regexType.GetField("_code", BindingFlags.NonPublic | BindingFlags.Instance);
+        var regexCode = regexCodeField?.GetValue(regex);
+
+        if (regexCode == null)
+            return null;
+
+        var regexCodeType = regexCode.GetType();
+        var trackCountField = regexCodeType.GetField("TrackCount", BindingFlags.Public | BindingFlags.Instance);
+
+        return trackCountField?.GetValue(regexCode) as int?;
+    }
+}
