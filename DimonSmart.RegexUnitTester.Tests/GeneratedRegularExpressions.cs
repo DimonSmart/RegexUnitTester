f91741f0cbfb7837d77390cd6ab6d5bfe7273b05
Dmitrii Dorogoi <dmitrii_dorogoi@epam.com>
2024-06-07 17:41:26 +0200

    GeneratedRegexAttribute test example added

---
+++ DimonSmart.RegexUnitTester.Tests/GeneratedRegularExpressions.cs
@@ -0,0 +1,13 @@
+﻿using DimonSmart.RegexUnitTester.Attributes;
+using System.Text.RegularExpressions;
+
+namespace DimonSmart.RegexUnitTester.Tests;
+
+public static partial class GeneratedRegularExpressions
+{
+    [GeneratedRegex("abc|def", RegexOptions.IgnoreCase, "en-US")]
+    [ShouldNotMatch("123_12-1234")]
+    [ShouldMatch("123-12-1234")]
+    private static partial Regex AbcOrDefGeneratedRegex();
+}
+
